8d9f7ecf338f2a693041441f3ba7e7e6f37cc14e
Daniil <myps4accaunt2001@gmail.com>
2022-10-27 22:54:43 +0300

    added environment variables: password and username

--- src/main/kotlin/com/example/javaauthserver/config/SpringSecurityConfiguration.java
+++ src/main/kotlin/com/example/javaauthserver/config/SpringSecurityConfiguration.java
@@ -1,13 +1,31 @@
 package com.example.javaauthserver.config;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
+@Configuration
 public class SpringSecurityConfiguration {
+
+    @Value("#{environment.USER_NAME}")
+    String username;
+    @Value("#{environment.PASSWORD}")
+    String password;
+
+    final static Logger logger = LogManager.getLogger(SpringSecurityConfiguration.class.getName());
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -17,6 +35,27 @@ public class SpringSecurityConfiguration {
                 .formLogin(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+
+        if(username.isEmpty() || password.isEmpty()){
+            logger.error("Set the user name and password. " +
+                    "This can be done by specifying the following parameters...");
+        }
+
+        UserDetails user = User.withUsername(username)
+                .password(passwordEncoder().encode(password))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
